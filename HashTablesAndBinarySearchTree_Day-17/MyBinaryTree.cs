d504a1b0a06edd2902150bab393d15a5e372de39
dheer meena <dheer1998meena@gmail.com>
2020-10-26 16:16:30 +0530

    [Dheer] Creating Binary Tree for adding element in the sorting order

---
+++ HashTablesAndBinarySearchTree_Day-17/MyBinaryTree.cs
@@ -0,0 +1,101 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AddressBook.cs" company="Bridgelabz">
+//   Copyright © 2018 Company
+// </copyright>
+// <creator Name="Dheer Singh Meena"/>
+// --------------------------------------------------------------------------------------------------------------------
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HashTablesAndBinarySearchTree_Day_17
+{
+    //Creating MyMapTree class as a generic class 
+   public class MyBinaryTree<T> where T : IComparable
+    {
+        // nodeData value in the Binary tree
+        public T nodeData { get; set; }
+        // leftTree storing the element less than the node data
+        public MyBinaryTree<T> leftTree { get; set; }
+        // rightTree storing the element greater than the node data.
+        public MyBinaryTree<T> rightTree { get; set; }
+        // Defining two local variable.
+        int leftCount;
+        int rightCount;
+        // Creating a parameterized constructor.
+        public MyBinaryTree(T nodeData)
+        {
+            this.nodeData = nodeData;
+            this.leftTree = null;
+            this.rightTree = null;
+        }
+
+        /// <summary>
+        ///UC1- Inset the element in the specified node depending upon the value passed by user as item
+        /// </summary>
+        /// <param name="item"></param>
+        public void Insert(T item)
+        {
+            // Creating a currentNodeValue variable copying the value of nodeData into currentNodeValue
+            T currentNodeValue = this.nodeData;
+            //comparing if the value entered is greater than current node or less than 
+            if (currentNodeValue.CompareTo(item)>0)
+            {
+                //if the item value less than currentNodeValue then adding into left tree
+                if (this.leftTree == null)
+                {
+                    this.leftTree = new MyBinaryTree<T>(item);
+                    Console.WriteLine("Inserting item on left is :" + item);
+                }
+                // Recursion remember first it will reach the end node and also traverse back to upper nodes
+                else
+                {
+                    this.leftTree.Insert(item);
+                }
+            }
+            else
+            {
+                //if the item value greater than currentNodeValue then adding into left tree
+                if (this.rightTree == null)
+                {
+                    this.rightTree = new MyBinaryTree<T>(item);
+                    Console.WriteLine("Inserting item on right is" + item);
+                }
+                // Recursion remember first it will reach the end node and also traverse back to upper nodes
+                else
+                {
+                    this.rightTree.Insert(item);
+                }
+            }
+        }
+        /// <summary>
+        /// Creating Display method for Binary Tree.
+        /// </summary>
+        public void Display()
+        {
+            //If the leftTree is not null then printing the values in that left tree 
+            if (this.leftTree!= null)
+            {
+                this.leftCount++;
+                //Using recursion reach to the last node of left tree and print it 
+                this.leftTree.Display();
+            }
+            // Printing the root element.
+            Console.WriteLine(this.nodeData.ToString());
+            //If the rightTree is not null then printing the values in that left tree.
+            if (this.rightTree != null)
+            {
+                this.rightCount++;
+                //Using recursion reach to the last node of left tree and print it 
+                this.rightTree.Display();
+            }
+        }
+        /// <summary>
+        /// Getiing the size of Binary Treee
+        /// </summary>
+        public void GetSize()
+        {
+            Console.WriteLine("size" + "  " + (1 + this.leftCount + this.rightCount));
+        }
+    }
+}
--- HashTablesAndBinarySearchTree_Day-17/Program.cs
+++ HashTablesAndBinarySearchTree_Day-17/Program.cs
@@ -12,24 +12,14 @@ namespace HashTablesAndBinarySearchTree_Day_17
     {
         static void Main(string[] args)
         {
-            // Creating an object of MyMapNode Class
-            // UC3 Ability to Remove particular words from large paragraph phrase
-            // Creating an object of MyMapNode Class
-            // size of the hash table is 20
-            // Storing the sentence in the variable para
-            MyMapNode<string, string> hash = new MyMapNode<string, string>(20); 
-            string para = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
-            string[] paraWords = para.Split(' ');  // spliting the words of the phrase and storing each word in the Array String
-           int pLength = paraWords.Length;  // Getting the length of the Array String
-            // Itreating along each word and adding it to hash set
-            for (int i = 0; i < pLength; i++)
-            {
-                hash.Add(Convert.ToString(i), paraWords[i]);
-            }
-            Console.WriteLine("Removing the word 'avoidable' from the hash table ");
-            Console.WriteLine("Frequency before removal of 'avoidable' :" + hash.GetFrequency("avoidable"));
-            hash.RemoveValue("avoidable");
-            Console.WriteLine("Frequency after removal of 'avoidable' :" + hash.GetFrequency("avoidable"));
+            //UC1- Inset the element in the specified node depending upon the value passed by user as item
+            // Creating a instance object of MyBinaryTree.
+            MyBinaryTree<int> binaryTree = new MyBinaryTree<int>(56);
+            Console.WriteLine("Inserting item");
+            binaryTree.Insert(30);
+            binaryTree.Insert(70);
+            binaryTree.Display();
+            binaryTree.GetSize();
             Console.ReadLine();
         }
     }
