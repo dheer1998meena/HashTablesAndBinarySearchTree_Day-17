374543e8df96fe92a4c43b798d8817b55acaaf85
dheer meena <dheer1998meena@gmail.com>
2020-10-26 07:44:56 +0530

    [Dheer] Creating hashtable to find frequency of words in large paragraph phrase

--- HashTablesAndBinarySearchTree_Day-17/Program.cs
+++ HashTablesAndBinarySearchTree_Day-17/Program.cs
@@ -12,23 +12,24 @@ namespace HashTablesAndBinarySearchTree_Day_17
     {
         static void Main(string[] args)
         {
+            // Creating an object of MyMapNode Class
             // UC_2- Ability to find frequency of words in large paragraph phrase
-            // Creating object of MyMapNode Class
-            // size of hash table is 20
+            // Creating an object of MyMapNode Class
+            // size of the hash table is 20
             MyMapNode<string, string> hash = new MyMapNode<string, string>(20);
-            // Storing the sentence in the variable paragraph
-            string paragraph = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
+            // Storing the sentence in the variable para
+            string para = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
             // spliting the words of the phrase and storing each word in the Array String
-            string[] paragraphWords = paragraph.Split(' ');
+            string[] paraWords = para.Split(' ');
             // Getting the length of the Array String
-            int paragraphLength = paragraphWords.Length;
+            int pLength = paraWords.Length;
             // Itreating along each word and adding it to hash set
-            for (int i = 0; i < paragraphLength; i++)
+            for (int i = 0; i < pLength; i++)
             {
-                hash.Add(Convert.ToString(i), paragraphWords[i]);
+                hash.Add(Convert.ToString(i), paraWords[i]);
             }
             // iterating using for each loop to get the frequency of each word in the sentence
-            foreach (string word in paragraphWords)
+            foreach (string word in paraWords)
             {
                 hash.GetFrequency(word);
             }
